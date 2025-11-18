612d9fee4ef94152161848cb53f1af0820d58154
akr4m <akram@futurestation.net>
2025-11-18 21:56:34 +0600

    feat: enhance user presence management and update user display in chat room

--- app/Livewire/Room/Users.php
+++ app/Livewire/Room/Users.php
@@ -2,10 +2,49 @@
 
 namespace App\Livewire\Room;
 
+use App\Models\Room;
+use App\Models\User;
+use Illuminate\Support\Arr;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Users extends Component
 {
+    public Room $room;
+
+    public array $ids = [];
+
+    public array $typingIds = [];
+
+    #[Computed()]
+    public function users()
+    {
+        return User::whereIn('id', $this->ids)->get();
+    }
+
+    #[On('echo-presence:chat.room.{room.id},here')]
+    public function setUsersHere($users)
+    {
+        $this->ids = Arr::pluck($users, 'id');
+    }
+
+    #[On('echo-presence:chat.room.{room.id},joining')]
+    public function setUsersJoining($user)
+    {
+        if (in_array($user['id'], $this->ids)) {
+            return;
+        }
+
+        $this->ids[] = $user['id'];
+    }
+
+    #[On('echo-presence:chat.room.{room.id},leaving')]
+    public function setUsersLeaving($user)
+    {
+        $this->ids = array_filter($this->ids, fn ($id) => $id !== $user['id']);
+    }
+
     public function render()
     {
         return view('livewire.room.users');
--- resources/views/livewire/room/users.blade.php
+++ resources/views/livewire/room/users.blade.php
@@ -2,15 +2,20 @@
     <h2 class="text-lg font-medium">Users</h2>
 
     <ul role="list" class="divide-y mt-4 divide-gray-100 dark:divide-white/5">
-        @for ($i = 0; $i < 3; $i++)
-            <li class="flex gap-x-2.5 py-3.5">
-            <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="size-10 flex-none rounded-full bg-gray-50 dark:bg-gray-800 dark:outline dark:-outline-offset-1 dark:outline-white/10" />
+        @foreach ($this->users as $user)
+        <li class="flex gap-x-2.5 py-3.5">
+            <flux:avatar tooltip="{{$user->name}}" name="{{$user->initials()}}" class="size-10 flex-none rounded-full bg-gray-50 dark:bg-gray-800 dark:outline dark:-outline-offset-1 dark:outline-white/10" />
             <div class="min-w-0">
-                <p class="text-sm/6 font-semibold text-gray-900 dark:text-white">Leslie Alexander</p>
-                <p class="truncate text-xs/5 text-gray-500 dark:text-gray-400">leslie.alexander@example.com</p>
+                <div class="inline-flex">
+                    <p class="text-sm/6 font-semibold text-gray-900 dark:text-white">{{ $user->name }}</p>
+                    @if(in_array($user->id, $typingIds))
+                    <span class="animate-pulse ml-2 text-xs text-green-800">typing...</span>
+                    @endif
+                </div>
+                <p class="truncate text-xs/5 text-gray-500 dark:text-gray-400">{{ $user->email }}</p>
             </div>
-            </li>
-            @endfor
+        </li>
+        @endforeach
     </ul>
 
 </div>
